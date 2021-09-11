3a8c64f74a5e396f4ae04a4a798d12c22bb4f0d3
LeeLin2602 <lin.cs09@nctu.edu.tw>
2021-09-12 02:23:51 +0800

    simplify nctu_oatuh module and improving security

--- modules/nctu_oauth/oauth.py
+++ modules/nctu_oauth/oauth.py
@@ -7,20 +7,16 @@ OAUTH_URL = 'https://id.nycu.edu.tw'
 class Oauth(object):
 	def __init__(self, redirect_uri, client_id, client_secret):
 		self.grant_type = 'authorization_code'
-		self.code = None
 		self.client_id = client_id
 		self.client_secret = client_secret
 		self.redirect_uri = redirect_uri
 
-	def get_code_url(self):
-		return OAUTH_URL + '/o/authorize/?client_id=' + self.client_id + '&scope=profile&response_type=code'
-
 	def get_token(self, code):
-		self.code = code
+
 		get_token_url = OAUTH_URL + '/o/token/'
 		data = {
 			'grant_type': 'authorization_code',
-			'code': self.code,
+			'code': code,
 			'client_id': self.client_id,
 			'client_secret': self.client_secret,
 			'redirect_uri': self.redirect_uri
